3cd27c492d62936800e906f47b460f4efb65c930
havembha <85038103+venkatbhat62@users.noreply.github.com>
2021-08-11 18:15:43 -0500

    Update JAGlobalLib.py

--- cgi-bin/JAGlobalLib.py
+++ cgi-bin/JAGlobalLib.py
@@ -2,6 +2,7 @@
     This module contains global functions used by JadooAudit
 
     GetGMTTime() - returns string with current GMT time the form YYYY/MM/DD hh:mm:ss.sss
+    JAYamlLoad(fileName) - reads the yaml file, returns data in dictionary
 
     Author: havembha@gmail.com, 2021-06-28
 """
@@ -35,4 +36,142 @@ def LogMsg(logMsg, fileName, appendDate=True):
         logFileStream.write( UTCDateTime() + " " + logMsg )
         logFileStream.close()
         return 1
+
+"""
+Basic function to read config file in yaml format
+Use this on host without python 3 or where yaml is not available
+
+"""
+def JAYamlLoad(fileName ):
+    from collections import defaultdict
+    import re
+    yamlData = defaultdict(dict)
+    paramNameAtDepth = {0: '', 1: '', 2: '', 3:'', 4: ''}
+    leadingSpacesAtDepth = {0: 0, 1: None, 2: None, 3: None, 4: None}
+    prevLeadingSpaces = 0
+    currentDepth = 0
+    currentDepthKeyValuePairs = defaultdict(dict)
+
+    try:
+        with open(fileName, "r") as file:
+            depth = 1
+
+            while True:
+                tempLine =  file.readline()
+                if not tempLine:
+                    break
+                ### SKIP comment line
+                if re.match(r'\s*#', tempLine):
+                    continue
+
+                tempLine = tempLine.rstrip("\n")
+                if re.match("---", tempLine) :
+                    continue
+                if len(tempLine) == 0:
+                    continue
+                ### remove leading and trailing spaces, newline
+                lstripLine = tempLine.lstrip()
+                if len(lstripLine) == 0:
+                    continue
+
+                ## separate param name and value, split to two parts, the value itself may have ':'
+                params = lstripLine.split(':', 1)
+                ## remove leading space from value field
+                params[1] = params[1].lstrip()
+
+                # based on leading spaces, determine depth
+                leadingSpaces = len(tempLine)-len(lstripLine)
+                if leadingSpaces == prevLeadingSpaces:
+
+                    if leadingSpaces == 0:
+                        if params[1] == None or len(params[1]) == 0 :
+                            ### if value does not exist, this is the start of parent/child definition
+                            paramNameAtDepth[currentDepth+1] = params[0]
+
+                        else:
+                            ### top layer, assign the key, value pair as is to yamlData
+                            yamlData[params[0]] = params[1]
+                    else:
+                        ### store key, value pair with current depth dictionary
+                        currentDepthKeyValuePairs[params[0]] = params[1]
+
+                    leadingSpacesAtDepth[currentDepth+1] = leadingSpaces
+
+                elif leadingSpaces < prevLeadingSpaces:
+                    ### store key, value pair of prev depth 
+                    for key, values in currentDepthKeyValuePairs.items():
+                        if currentDepth == 1:
+                            if paramNameAtDepth[1] not in yamlData.keys() :
+                                yamlData[ paramNameAtDepth[1]] = {}
+                            
+                            yamlData[ paramNameAtDepth[1] ][key] = values
+
+                        elif currentDepth == 2:
+                            if paramNameAtDepth[1] not in yamlData.keys() :
+                                yamlData[ paramNameAtDepth[1]] = {}
+                            if paramNameAtDepth[2] not in yamlData[paramNameAtDepth[1]].keys() :
+                                yamlData[ paramNameAtDepth[1]][paramNameAtDepth[2]] = {}
+
+                            yamlData[paramNameAtDepth[1]][paramNameAtDepth[2]][key] = values
+                        elif currentDepth == 3:
+                            if paramNameAtDepth[1] not in yamlData.keys() :
+                                yamlData[ paramNameAtDepth[1]] = {}
+                            if paramNameAtDepth[2] not in yamlData[paramNameAtDepth[1]].keys() :
+                                yamlData[ paramNameAtDepth[1]][paramNameAtDepth[2]] = {}
+                            if paramNameAtDepth[3] not in yamlData[paramNameAtDepth[1]][paramNameAtDepth[2]].keys() :
+                                yamlData[ paramNameAtDepth[1]][paramNameAtDepth[2]][paramNameAtDepth[3]] = {}
+                            yamlData[paramNameAtDepth[1]][paramNameAtDepth[2]][paramNameAtDepth[3]][key] = values
+
+                    currentDepthKeyValuePairs = defaultdict(dict)
+                    
+                    if leadingSpacesAtDepth[currentDepth-1] == leadingSpaces:
+                        currentDepth -= 1
+                    elif leadingSpacesAtDepth[currentDepth-2] == leadingSpaces:
+                        currentDepth -= 2
+                    elif leadingSpacesAtDepth[currentDepth-3] == leadingSpaces:
+                        currentDepth -= 3
+                    prevLeadingSpaces = leadingSpaces
+
+                    if params[1] == None or len(params[1]) == 0 :
+                        ### if value does not exist, this is the start of parent/child definition
+                        paramNameAtDepth[currentDepth+1] = params[0]
+                elif leadingSpaces > prevLeadingSpaces:
+                    leadingSpacesAtDepth[currentDepth+1] = leadingSpaces
+                    currentDepth += 1
+                    prevLeadingSpaces = leadingSpaces
+                    if params[1] == None or len(params[1]) == 0 :
+                        ### if value does not exist, this is the start of parent/child definition
+                        paramNameAtDepth[currentDepth+1] = params[0]
+                    else:
+                        ### save current key, value 
+                        currentDepthKeyValuePairs[params[0]] = params[1]
+
+            for key, values in currentDepthKeyValuePairs.items():
+                if currentDepth == 1:
+                    if paramNameAtDepth[1] not in yamlData.keys() :
+                        yamlData[ paramNameAtDepth[1]] = {}
+                            
+                    yamlData[ paramNameAtDepth[1] ][key] = values
+
+                elif currentDepth == 2:
+                    if paramNameAtDepth[1] not in yamlData.keys() :
+                        yamlData[ paramNameAtDepth[1]] = {}
+                    if paramNameAtDepth[2] not in yamlData[paramNameAtDepth[1]].keys() :
+                        yamlData[ paramNameAtDepth[1]][paramNameAtDepth[2]] = {}
+
+                    yamlData[paramNameAtDepth[1]][paramNameAtDepth[2]][key] = values
+                elif currentDepth == 3:
+                    if paramNameAtDepth[1] not in yamlData.keys() :
+                        yamlData[ paramNameAtDepth[1]] = {}
+                    if paramNameAtDepth[2] not in yamlData[paramNameAtDepth[1]].keys() :
+                        yamlData[ paramNameAtDepth[1]][paramNameAtDepth[2]] = {}
+                    if paramNameAtDepth[3] not in yamlData[paramNameAtDepth[1]][paramNameAtDepth[2]].keys() :
+                        yamlData[ paramNameAtDepth[1]][paramNameAtDepth[2]][paramNameAtDepth[3]] = {}
+                    yamlData[paramNameAtDepth[1]][paramNameAtDepth[2]][paramNameAtDepth[3]][key] = values
+            file.close()
+            return yamlData
+
+    except OSError as err:
+        print('ERROR Can not read file:|' + fileName + '|, ' + "OS error: {0}".format(err) + '\n')
+        return yamlData
 
