cfe33881db14674ecb4e93d6f22bebc974b89f0e
havembha <85038103+venkatbhat62@users.noreply.github.com>
2021-08-28 17:27:25 -0500

    Update JAGatherOSStats.py

--- client/JAGatherOSStats.py
+++ client/JAGatherOSStats.py
@@ -303,8 +303,46 @@ if debugLevel > 0:
         fsNames = spec[1] 
         print('DEBUG-1 Name: {0}, Fields: {1}, fsNames: {2}'.format(key, fields, fsNames))
 
+import platform
 ### if another instance is running, exit
-import subprocess,platform
+try:
+    from subprocess import CompletedProcess
+except ImportError:
+    # Python 2
+    class CompletedProcess:
+
+        def __init__(self, args, returncode, stdout=None, stderr=None):
+            self.args = args
+            self.returncode = returncode
+            self.stdout = stdout
+            self.stderr = stderr
+
+        def check_returncode(self):
+           if self.returncode != 0:
+           err = subprocess.CalledProcessError(self.returncode, self.args, output=self.stdout)
+           raise err
+           return self.returncode
+
+        def sp_run(*popenargs, **kwargs):
+            input = kwargs.pop("input", None)
+            check = kwargs.pop("handle", False)
+            if input is not None:
+                if 'stdin' in kwargs:
+                    raise ValueError('stdin and input arguments may not both be used.')
+                kwargs['stdin'] = subprocess.PIPE
+            process = subprocess.Popen(*popenargs, **kwargs)
+            try:
+                outs, errs = process.communicate(input)
+            except:
+                process.kill()
+                process.wait()
+                raise
+                returncode = process.poll()
+             if check and returncode:
+                raise subprocess.CalledProcessError(returncode, popenargs, output=outs)
+             return CompletedProcess(popenargs, returncode, stdout=outs, stderr=errs)
+         subprocess.run = sp_run
+         # ^ This monkey patch allows it work on Python 2 or 3 the same way
 
 if platform.system() == 'Windows':
     result =  subprocess.run(['tasklist'],stdout=subprocess.PIPE,stderr=subprocess.DEVNULL)
