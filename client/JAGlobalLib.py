8592c1fb61ba21a93ae54e8dc64d629abe12c0b2
havembha <85038103+venkatbhat62@users.noreply.github.com>
2021-08-31 20:15:38 -0500

    Update JAGlobalLib.py

--- client/JAGlobalLib.py
+++ client/JAGlobalLib.py
@@ -325,6 +325,45 @@ def JAPostJSONData(URL,data, verifyCertificate, headers, disableWarnings=True, d
         print('INIFO JAPostJSONData() returnStatus:{0}'.format( returnStatus.text ))
             
     else:
+        try:
+            from subprocess import CompletedProcess
+        except ImportError:
+            # Python 2
+            class CompletedProcess:
+
+                def __init__(self, args, returncode, stdout=None, stderr=None):
+                    self.args = args
+                    self.returncode = returncode
+                    self.stdout = stdout
+                    self.stderr = stderr
+
+                def check_returncode(self):
+                    if self.returncode != 0:
+                        err = subprocess.CalledProcessError(self.returncode, self.args, output=self.stdout)
+                        raise err
+                        return self.returncode
+
+                def sp_run(*popenargs, **kwargs):
+                    input = kwargs.pop("input", None)
+                    check = kwargs.pop("handle", False)
+                    if input is not None:
+                        if 'stdin' in kwargs:
+                            raise ValueError('stdin and input arguments may not both be used.')
+                        kwargs['stdin'] = subprocess.PIPE
+                    process = subprocess.Popen(*popenargs, **kwargs)
+                    try:
+                        outs, errs = process.communicate(input)
+                    except:
+                        process.kill()
+                        process.wait()
+                        raise
+                    returncode = process.poll()
+                    if check and returncode:
+                        raise subprocess.CalledProcessError(returncode, popenargs, output=outs)
+                    return CompletedProcess(popenargs, returncode, stdout=outs, stderr=errs)
+                
+                subprocess.run = sp_run
+
         result =  subprocess.run(['curl', '-X', 'POST', URL, '-H', 'Content-Type: application/json', '-d', data],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
         returnStatus = result.stdout.decode('utf-8').split('\n')
         print('INFO JAPostJSONData() returnStatus:{0}'.format( returnStatus ))
